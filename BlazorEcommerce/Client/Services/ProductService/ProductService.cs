661f2bd8566870e525433b6b690e99c551576a97
microsoftv3 <stronghuman0207@gmail.com>
2023-12-02 19:12:24 -0300

    Client side - Get Products

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -14,17 +14,22 @@ namespace BlazorEcommerce.Client.Services.ProductServices
         }
         public List<Product> Products { get ; set; } = new List<Product>();
 
+        public event Action ProductsChanged;
+
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
             return result;
         }
 
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
-            if(result != null && result.Data != null)
+            var result = categoryUrl == null ?
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product") :
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+            if (result != null && result.Data != null)
                  Products = result.Data;
+            ProductsChanged.Invoke();
         }
     }
 }
