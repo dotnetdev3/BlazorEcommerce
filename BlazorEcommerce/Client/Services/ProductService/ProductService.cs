9d59c7e76b4a39156e3505c9cbab37a29667cbe9
microsoftv3 <stronghuman0207@gmail.com>
2023-12-08 23:41:25 -0300

    Implement the Search Component

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -13,6 +13,7 @@ namespace BlazorEcommerce.Client.Services.ProductServices
             _http = http;
         }
         public List<Product> Products { get ; set; } = new List<Product>();
+        public string Message { get; set; } = "loading products...";
 
         public event Action ProductsChanged;
 
@@ -29,6 +30,23 @@ namespace BlazorEcommerce.Client.Services.ProductServices
                 await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
             if (result != null && result.Data != null)
                  Products = result.Data;
+            ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "No products found.";
             ProductsChanged.Invoke();
         }
     }
