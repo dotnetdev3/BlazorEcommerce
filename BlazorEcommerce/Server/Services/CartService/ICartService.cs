05b2b4f14f6fcc527618f6f60ffbb389b618ab31
microsoftv3 <stronghuman0207@gmail.com>
2024-01-03 14:12:24 -0300

    Get the Number of Cart Items from the Server

--- BlazorEcommerce/Server/Services/CartService/ICartService.cs
+++ BlazorEcommerce/Server/Services/CartService/ICartService.cs
@@ -6,5 +6,6 @@ namespace BlazorEcommerce.Server.Services.CartService
     {
        Task<ServiceResponse<List<CartProductRespose>>> GetCartProducts(List<CartItem> cartItems);
        Task<ServiceResponse<List<CartProductRespose>>> StoreCartItems(List<CartItem> cartItems);
+       Task<ServiceResponse<int>> GetCartItemsCount();
     }
 }
