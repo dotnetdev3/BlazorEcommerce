c34bbea0f0fe70297a61b550f3c125f46d312377
microsoftv3 <stronghuman0207@gmail.com>
2024-01-11 22:34:15 -0300

    Show Order Details on the Client

--- BlazorEcommerce/Client/Services/OrderService/OrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/OrderService.cs
@@ -17,6 +17,12 @@ namespace BlazorEcommerce.Client.Services.OrderService
             _authenticationStateProvider = authenticationStateProvider;
             _navigationManager = navigationManager;
         }
+        
+        public async Task<OrderDetailsResponse> GetOrderDetails(int orderId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<OrderDetailsResponse>>($"api/order/{orderId}");
+            return result.Data;
+        }
 
         public async Task PlaceOrder()
         {
