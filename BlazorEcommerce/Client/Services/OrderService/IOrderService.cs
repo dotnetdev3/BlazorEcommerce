923b3e0907dbf35948ea339659467cea31775558
microsoftv3 <stronghuman0207@gmail.com>
2024-01-14 16:29:17 -0300

    Redirect the User to a Stripe Checkout Page

--- BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
+++ BlazorEcommerce/Client/Services/OrderService/IOrderService.cs
@@ -4,7 +4,7 @@ namespace BlazorEcommerce.Client.Services.OrderService
 {
     public interface IOrderService
     {
-        Task PlaceOrder();
+        Task<string> PlaceOrder();
         Task<List<OrderOverviewResponse>> GetOrders();
         Task<OrderDetailsResponse> GetOrderDetails(int orderId);
     }
