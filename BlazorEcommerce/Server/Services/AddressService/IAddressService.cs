7030f59385e8d6d99f32a2e030234837f27d11c0
microsoftv3 <stronghuman0207@gmail.com>
2024-01-20 16:24:16 -0300

    Implement the Address Logic on the Server

---
+++ BlazorEcommerce/Server/Services/AddressService/IAddressService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Server.Services.AddressService
+{
+    public interface IAddressService
+    {
+        Task<ServiceResponse<Address>> GetAddress();
+        Task<ServiceResponse<Address>> AddOrUpdateAddress(Address address);
+
+    }
+}
