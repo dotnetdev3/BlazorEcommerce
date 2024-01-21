71cf8ecf5c511399c2890e228b447a5da54132cd
microsoftv3 <stronghuman0207@gmail.com>
2024-01-21 20:45:51 -0300

    Implement the Address Logic on the Client

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerce.Client.Services.AuthService;
 using Microsoft.AspNetCore.Components.Authorization;
 using BlazorEcommerce.Client.Services.OrderService;
+using BlazorEcommerce.Client.Services.AddressService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -19,6 +20,7 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddOptions();
---
+++ BlazorEcommerce/Client/Services/AddressService/IAddressService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services.AddressService
+{
+    public interface IAddressService
+    {
+        Task<Address> GetAddress();
+        Task<Address> AddOrUpdateAddress(Address address);
+
+    }
+}
